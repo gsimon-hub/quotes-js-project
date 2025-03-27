24b69eaf5107c3a7524070cfd5bfdc0874e891ce
gsimon-hub <xiongwei_gao@outlook.com>
2025-03-27 09:21:40 +0800

    playwright for quotes js 1st page

--- quotes_js_scraper/spiders/quotes.py
+++ quotes_js_scraper/spiders/quotes.py
@@ -1,11 +1,36 @@
 import scrapy
-from quotes_js_scraper.items import QuoteItem
+from ..items import QuoteItem
+from scrapy_playwright.page import PageMethod
 
 
 class QuotesSpider(scrapy.Spider):
-    name = 'quotes'
-    allowed_domains = ['quotes.toscrape.com']
-    start_urls = ['http://quotes.toscrape.com/']
+    name = "quotes"
+    # allowed_domains = ["quotes.toscrape.com"]
+    # start_urls = ["http://quotes.toscrape.com/"]
+
+    def start_requests(self):
+        url = "https://quotes.toscrape.com/js/"
+        # yield scrapy.Request(url, meta={"playwright": True})
+        yield scrapy.Request(
+            url,
+            meta=dict(
+                playwright=True,
+                playwright_include_page=True,
+                playwright_page_methods=[PageMethod("waitForSelector", "div.quote")],
+                errback=self.errback,
+            ),
+        )
 
     def parse(self, response):
-        pass
+        for quote in response.css("div.quote"):
+            quote_item = QuoteItem()
+            quote_item["text"] = quote.css("span.text::text").get()
+            quote_item["author"] = quote.css("span small.author::text").get()
+            quote_item["tags"] = quote.css("div.tags a.tag::text").getall()
+
+            yield quote_item
+
+    async def errback(self, failure):
+        page = failure.request.meta["playwright_page"]
+        # self.logger.error(repr(failure))
+        await page.close()
